42f0db2b8850b2b7fd4677889ecf57ce57103f55
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2023-08-16 01:18:28 +0700

    [khoa] revert code 017-aws-cwagent-xray for java

--- 017-aws-cwagent-xray-java/src/main/java/com/vntechies/awscloudwatchagentforawsxray/services/IObjectService.java
+++
@@ -1,5 +0,0 @@
-package com.vntechies.awscloudwatchagentforawsxray.services;
-
-public interface IObjectService {
-    Object putObject(String key, byte[] bytes);
-}
--- 017-aws-cwagent-xray-java/src/main/java/com/vntechies/awscloudwatchagentforawsxray/services/ISongService.java
+++
@@ -1,8 +0,0 @@
-package com.vntechies.awscloudwatchagentforawsxray.services;
-
-import com.vntechies.awscloudwatchagentforawsxray.entities.SongEntity;
-
-public interface ISongService {
-
-    SongEntity add(SongEntity songEntity);
-}
